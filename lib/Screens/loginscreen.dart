8871447259821ffdf81fda5e8d77d6a8160101d6
Utsavrai <Utsavrai890@gmail.com>
2023-01-04 03:12:29 +0530

    added login screen

---
+++ lib/Screens/loginscreen.dart
@@ -0,0 +1,178 @@
+// ignore_for_file: prefer_const_constructors, prefer_final_fields, unused_field
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:rentapp/library/imageviewer.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool isVisible = true;
+  bool visible = false;
+  bool _isVisible = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CommonImageView(
+                imagePath: 'assets/gif/login.gif',
+                height: MediaQuery.of(context).size.height * (0.5),
+                width: MediaQuery.of(context).size.width,
+              ),
+              Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Please Login to continue',
+                style: TextStyle(
+                    color: Colors.grey.shade600,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.black, fontSize: 18),
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.alternate_email_rounded),
+                    filled: true,
+                    fillColor: Colors.grey.shade200,
+                    contentPadding: EdgeInsets.only(right: 20, left: 20),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none),
+                    hintStyle:
+                        TextStyle(fontSize: 16, color: Colors.grey.shade700),
+                    hintText: "Email-id"),
+                validator: (email) =>
+                    email != null && !EmailValidator.validate(email)
+                        ? 'Enter a valid email'
+                        : null,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                obscureText: !_isVisible,
+                style: TextStyle(color: Colors.black, fontSize: 20),
+                decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.password_rounded),
+                    suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          _isVisible = !_isVisible;
+                        });
+                      },
+                      icon: _isVisible
+                          ? Icon(
+                              Icons.visibility,
+                              color: Colors.grey.shade500,
+                            )
+                          : Icon(
+                              Icons.visibility_off,
+                              color: Colors.grey.shade500,
+                            ),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey.shade200,
+                    contentPadding: EdgeInsets.only(right: 20, left: 20),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide.none),
+                    hintStyle:
+                        TextStyle(fontSize: 16, color: Colors.grey.shade700),
+                    hintText: "Password"),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                margin: EdgeInsets.only(right: 40, left: 40),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Colors.deepPurple.shade400,
+                    Colors.deepPurple.shade400
+                  ]),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(10),
+                  ),
+                ),
+                width: 400,
+                child: TextButton.icon(
+                  style: TextButton.styleFrom(
+                    textStyle:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.login_rounded,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 18),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Forgot',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: ' Password?',
+                        style:
+                            TextStyle(color: Colors.deepPurple, fontSize: 20),
+                        recognizer: TapGestureRecognizer()..onTap = () {},
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Don\'t have an account?',
+                    style: TextStyle(color: Colors.black, fontSize: 20),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: ' Sign up',
+                          style:
+                              TextStyle(color: Colors.deepPurple, fontSize: 20),
+                          recognizer: TapGestureRecognizer()..onTap = () {})
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
