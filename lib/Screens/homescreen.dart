54a64ee6ddec8d01ab0e77de112604c9e287e01e
Utsavrai <Utsavrai890@gmail.com>
2023-01-06 14:06:34 +0530

    Resolved errors

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -26,7 +26,7 @@ class _HomeScreenState extends State<HomeScreen> {
             snap: false,
             centerTitle: true,
             title: Text(
-              'Rent Monk',
+              'Rent',
               style: TextStyle(
                   fontFamily: 'IndieFlower',
                   fontSize: 30,
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,8 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+// ignore_for_file: prefer_const_constructors, unused_import
+
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -13,6 +15,6 @@ import 'package:rentapp/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
   });
 }
